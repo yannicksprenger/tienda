435b9fc1e47419c28da3037047c2d8958a494bef
Alejandro De la Cruz Tarin <ale.tarin10@gmail.com>
2018-04-05 09:07:32 -0500

    Hardcoded home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+
+  slides = [
+    {
+      title: "Cigarros electricos",
+      description: "Diferentes modelos para elegir, en diferentes colores",
+      image: "../../assets/imgs/cigarros.jpg",
+    },
+    {
+      title: "Kit especial",
+      description: "Oferta con kit especial que contiene todo lo que necesitas para disfrutar tu nuevo cigarro electrico",
+      image: "../../assets/imgs/kit.jpg",
+    },
+    {
+      title: "Liquidos de diferentes sabores",
+      description: "Tenemos una variedad de sabores para elegir, disfruta de la variedad",
+      image: "../../assets/imgs/sabores.jpg",
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ngOnInit(): void {
 
   }
 
